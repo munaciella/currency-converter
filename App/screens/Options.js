e7621cb78d6d6ab741205511047543e8d295447b
munaciella <franes88@gmail.com>
2024-03-16 00:20:39 +0000

    add icons to initial page

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -6,13 +6,17 @@ import {
   SafeAreaView,
   StyleSheet,
 } from 'react-native';
-
+import Icon from 'react-native-vector-icons/FontAwesome';
 import colors from '../constants/colors';
 
 const styles = StyleSheet.create({
   row: {
     paddingHorizontal: 20,
     paddingVertical: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomColor: colors.border,
+    justifyContent:'space-between',
   },
   text: {
     fontSize: 16,
@@ -30,18 +34,21 @@ export default () => {
     <SafeAreaView>
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>Themes</Text>
+        <Icon name="chevron-right" size={20} color={colors.blue} />
       </TouchableOpacity>
 
       <View style={styles.separator} />
 
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>React Native Basics</Text>
+        <Icon name="external-link" size={20} color={colors.blue} />
       </TouchableOpacity>
 
       <View style={styles.separator} />
 
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>React Native by Example</Text>
+        <Icon name="external-link" size={20} color={colors.blue} />
       </TouchableOpacity>
     </SafeAreaView>
   );
