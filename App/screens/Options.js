a8c59bbe9e8e6538b3473cf3921de97b84225392
munaciella <franes88@gmail.com>
2024-03-18 23:37:11 +0000

    add a row item component

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,57 +1,34 @@
 import React from 'react';
-import {
-  View,
-  TouchableOpacity,
-  Text,
-  SafeAreaView,
-  StyleSheet,
-} from 'react-native';
+import { SafeAreaView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 //import { Feather } from '@expo/vector-icons';
 import colors from '../constants/colors';
-
-const styles = StyleSheet.create({
-  row: {
-    paddingHorizontal: 20,
-    paddingVertical: 16,
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderBottomColor: colors.border,
-    justifyContent:'space-between',
-  },
-  text: {
-    fontSize: 16,
-    color: colors.text,
-  },
-  separator: {
-    backgroundColor: colors.border,
-    height: StyleSheet.hairlineWidth,
-    marginLeft: 20
-  }
-})
+import { RowItem, RowSeparator } from '../components/RowItem';
 
 export default () => {
   return (
     <SafeAreaView>
-      <TouchableOpacity style={styles.row}>
-        <Text style={styles.text}>Themes</Text>
-        <Icon name="chevron-right" size={20} color={colors.blue} />
-      </TouchableOpacity>
+      <RowItem
+        text="Themes"
+        onPress={() => alert('todo!')}
+        rightIcon={<Icon name="chevron-right" size={20} color={colors.blue} />}
+      />
 
-      <View style={styles.separator} />
+      <RowSeparator />
 
-      <TouchableOpacity style={styles.row}>
-        <Text style={styles.text}>React Native Basics</Text>
-        <Icon name="external-link" size={20} color={colors.blue} />
-      </TouchableOpacity>
+      <RowItem
+        text="React Native Basics"
+        onPress={() => alert('todo!')}
+        rightIcon={<Icon name="external-link" size={20} color={colors.blue} />}
+      />
 
-      <View style={styles.separator} />
+      <RowSeparator />
 
-      <TouchableOpacity style={styles.row}>
-        <Text style={styles.text}>React Native by Example</Text>
-        <Icon name="external-link" size={20} color={colors.blue} />
-      </TouchableOpacity>
-
+      <RowItem
+        text="React Native by Example"
+        onPress={() => alert('todo!')}
+        rightIcon={<Icon name="external-link" size={20} color={colors.blue} />}
+      />
     </SafeAreaView>
   );
 };
