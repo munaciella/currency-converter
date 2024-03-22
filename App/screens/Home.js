a5c5dd63c161f9fe715071ca2017c87632397d50
munaciella <franes88@gmail.com>
2024-03-22 23:19:51 +0000

    add home comp and android statusbar

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,1 +1,18 @@
-export default () => null 
+import React from 'react';
+import { View, StyleSheet, StatusBar } from 'react-native';
+import colors from '../constants/colors';
+
+const styles = StyleSheet.create({
+    container: {    
+    backgroundColor: colors.blue,
+    flex: 1,
+    }
+})
+
+export default () => {
+    return (
+        <View style={styles.container}>
+            <StatusBar barStyle="light-content" backgroundColor={colors.blue}/>
+        </View>
+    )
+}
