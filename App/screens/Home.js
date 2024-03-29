c42be29067b325a6cf0ee086f6eaf52958f5d5fd
munaciella <franes88@gmail.com>
2024-03-29 00:22:02 +0000

    add text and date to main page

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { View, StyleSheet, StatusBar, Image, Dimensions } from 'react-native';
+import { View, StyleSheet, StatusBar, Image, Dimensions, Text } from 'react-native';
 import colors from '../constants/colors';
 import { ConversionInput } from '../components/ConversionInput';
+import { format } from 'date-fns'
 
 const screen = Dimensions.get('window');
 
@@ -25,9 +26,26 @@ const styles = StyleSheet.create({
     width: screen.width * 0.25,
     height: screen.width * 0.25,
   },
+  textHeader: {
+    color: colors.white,
+    fontWeight: "bold",
+    fontSize: 30,
+    marginVertical: 20,
+    textAlign: "center"
+  },
+  text: {
+    color: colors.white,
+    fontSize: 14,
+    textAlign: "center",
+  },
 });
 
 export default () => {
+  const baseCurrency = 'USD'
+  const quoteCurrency = 'GBP'
+  const conversionRate = 0.8345
+  const date = new Date()
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor={colors.blue} />
@@ -43,19 +61,24 @@ export default () => {
           resizeMode="contain"
         />
       </View>
+
+      <Text style={styles.textHeader}>Currency Converter</Text>
       <ConversionInput
-        text="USD"
+        text={baseCurrency}
         value="123"
         onButtonPress={() => alert("todo!")}
         keyboardType="numeric"
         onChangeText={text => console.log("text", text)}
       />
       <ConversionInput
-        text="GBP"
+        text={quoteCurrency}
         value="123"
         editable={false}
         onButtonPress={() => alert("todo!")}
       />
+      <Text style={styles.text}>
+      {`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${format(date, 'do MMMM yyyy')}.`}        
+</Text>
     </View>
   );
 };
