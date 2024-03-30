b79a35300fbeb1fc97fc6943a92c90f38194dc73
munaciella <franes88@gmail.com>
2024-03-30 00:22:34 +0000

    add reverse currency button

--- App/components/Button.js
+++ App/components/Button.js
@@ -1,1 +1,14 @@
-export const Button = () => null
+import React from 'react';
+import { TouchableOpacity, Image, StyleSheet, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+
+});
+export const Button = ({ onPress, text }) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Image />
+        <Text>{text}</Text>
+    </TouchableOpacity>
+  );
+};
