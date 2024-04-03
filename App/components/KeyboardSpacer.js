a810d0fe37054b662964df392a0c00246cf81a95
munaciella <franes88@gmail.com>
2024-04-03 23:12:01 +0100

    add react navigation

--- App/components/KeyboardSpacer.js
+++ App/components/KeyboardSpacer.js
@@ -14,15 +14,15 @@ export const KeyboardSpacer = ({onToggle}) => {
 
   useEffect(() => {
     const showListener = Keyboard.addListener('keyboardDidShow', event => {
-        console.log(event)
         const screenHeight = Dimensions.get('window').height
         const endY = event.endCoordinates.screenY
 
-        setKeyboardSpace(screenHeight - endY)
+        setKeyboardSpace(screenHeight - endY + 20)
         onToggle(true)
     });
 
     const hideListener = Keyboard.addListener('keyboardDidHide', () => {
+        setKeyboardSpace(0)
         onToggle(false)
     });
 
