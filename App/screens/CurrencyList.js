9f4aa6c48ea8a7e389f5c72d1fa4f770dd378cb0
munaciella <franes88@gmail.com>
2024-04-06 23:50:48 +0100

    fix the status bar on currency list

--- App/screens/CurrencyList.js
+++ App/screens/CurrencyList.js
@@ -1,1 +1,7 @@
-export default () => null
+import React from 'react';
+import { StatusBar } from 'react-native';
+import colors from '../constants/colors';
+
+export default () => (
+  <StatusBar barStyle="dark-content" backgroundColor={colors.white} />
+);
