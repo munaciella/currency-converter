4ba57b61d60ab69706c85be13d2414131e19bf8d
munaciella <franes88@gmail.com>
2024-04-09 00:06:40 +0100

    add safe area view

--- App/screens/CurrencyList.js
+++ App/screens/CurrencyList.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { StatusBar, FlatList, View } from 'react-native';
+import { StatusBar, FlatList, View, SafeAreaView } from 'react-native';
 import colors from '../constants/colors';
-import currencies from '../data/currencies.json'
+import currencies from '../data/currencies.json';
 import { RowItem, RowSeparator } from '../components/RowItem';
 
-export default () => (
-    <View style={{backgroundColor: colors.white}}>
-  <StatusBar barStyle="dark-content" backgroundColor={colors.white} />
-  <FlatList
-  data={currencies}
-  renderItem={({item}) => {
-    return (
-        <RowItem
-        text={item}
-        />
-    )
-  }}
-  keyExtractor={(item) => item}
-  ItemSeparatorComponent={() => <RowSeparator />}
-  />
-  </View>
-);
+export default () => {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: colors.white }}>
+      <StatusBar barStyle="dark-content" backgroundColor={colors.white} />
+      <FlatList
+        data={currencies}
+        renderItem={({ item }) => {
+          return <RowItem text={item} />;
+        }}
+        keyExtractor={(item) => item}
+        ItemSeparatorComponent={() => <RowSeparator />}
+      />
+    </SafeAreaView>
+  );
+};
