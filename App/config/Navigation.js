bbfc672a963eb53ec65de348abfdd405b11af27d
munaciella <franes88@gmail.com>
2024-04-12 23:08:51 +0100

    add modal screen and remove stack screen

--- App/config/Navigation.js
+++ App/config/Navigation.js
@@ -1,26 +1,40 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import Home from "../screens/Home";
-import Options from '../screens/Options'
-import CurrencyList from "../screens/CurrencyList";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from '../screens/Home';
+import Options from '../screens/Options';
+import CurrencyList from '../screens/CurrencyList';
 
-const MainStack = createStackNavigator()
+const MainStack = createStackNavigator();
 const MainStackScreen = () => (
-    <MainStack.Navigator 
-    // headerShown='false' 
-    //initialRouteName="CurrencyList"
-    >
-        <MainStack.Screen name="Home" component={Home} options={{headerShown: false}}/>
-        <MainStack.Screen name="Options" component={Options} />
-        <MainStack.Screen name="CurrencyList" component={CurrencyList} options={({route}) => ({
-            title: route.params.title,
-        })}/>
-    </MainStack.Navigator>
-)
+  <MainStack.Navigator
+  // headerShown='false'
+  //initialRouteName="CurrencyList"
+  >
+    <MainStack.Screen
+      name="Home"
+      component={Home}
+      options={{ headerShown: false }}
+    />
+    <MainStack.Screen name="Options" component={Options} />
+  </MainStack.Navigator>
+);
+
+const ModalStack = createStackNavigator();
+const ModalStackScreen = () => (
+  <ModalStack.Navigator>
+    <ModalStack.Screen
+      name="CurrencyList"
+      component={CurrencyList}
+      options={({ route }) => ({
+        title: route.params && route.params.title,
+      })}
+    />
+  </ModalStack.Navigator>
+);
 
 export default () => (
-    <NavigationContainer>
-        <MainStackScreen />
-    </NavigationContainer>
-)
+  <NavigationContainer>
+    <MainStackScreen />
+  </NavigationContainer>
+);
