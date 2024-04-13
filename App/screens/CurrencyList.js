5356a3cd3dbe10b12728a7282c02bccad4652783
munaciella <franes88@gmail.com>
2024-04-13 22:25:25 +0100

    add check icon to currency

--- App/screens/CurrencyList.js
+++ App/screens/CurrencyList.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { StatusBar, FlatList, View, SafeAreaView } from 'react-native';
+import { StatusBar, FlatList, SafeAreaView, StyleSheet, View } from 'react-native';
 import colors from '../constants/colors';
 import currencies from '../data/currencies.json';
 import { RowItem, RowSeparator } from '../components/RowItem';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+const styles = StyleSheet.create({
+
+})
 
 export default ({navigation}) => {
   return (
@@ -11,7 +16,11 @@ export default ({navigation}) => {
       <FlatList
         data={currencies}
         renderItem={({ item }) => {
-          return <RowItem text={item} onPress={() => navigation.pop()}/>;
+          return <RowItem text={item} onPress={() => navigation.pop()} rightIcon={
+            <View style={styles.icon}>
+             <FontAwesome5 name="check" size={20} color={colors.white} />   
+            </View>
+          }/>;
         }}
         keyExtractor={(item) => item}
         ItemSeparatorComponent={() => <RowSeparator />}
