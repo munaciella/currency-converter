6c3033641ea9cb25164c9aae8779211b9f341359
munaciella <franes88@gmail.com>
2024-04-14 23:54:25 +0100

    add active currency

--- App/screens/CurrencyList.js
+++ App/screens/CurrencyList.js
@@ -1,33 +1,50 @@
 import React from 'react';
-import { StatusBar, FlatList, SafeAreaView, StyleSheet, View } from 'react-native';
+import {
+  StatusBar,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  View,
+} from 'react-native';
 import colors from '../constants/colors';
 import currencies from '../data/currencies.json';
 import { RowItem, RowSeparator } from '../components/RowItem';
 import { FontAwesome5 } from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
-    icon: {
-        width: 30,
-        height: 30,
-        backgroundColor: colors.blue,
-        borderRadius: 15,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+  icon: {
+    width: 30,
+    height: 30,
+    backgroundColor: colors.blue,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
-export default ({navigation}) => {
+export default ({ navigation, route = {} }) => {
+  const params = route.params || {};
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: colors.white }}>
       <StatusBar barStyle="dark-content" backgroundColor={colors.white} />
       <FlatList
         data={currencies}
         renderItem={({ item }) => {
-          return <RowItem text={item} onPress={() => navigation.pop()} rightIcon={
-            <View style={styles.icon}>
-             <FontAwesome5 name="check" size={20} color={colors.white} />   
-            </View>
-          }/>;
+          const selected = params.activeCurrency === item;
+          return (
+            <RowItem
+              text={item}
+              onPress={() => navigation.pop()}
+              rightIcon={
+                selected && (
+                  <View style={styles.icon}>
+                    <FontAwesome5 name="check" size={20} color={colors.white} />
+                  </View>
+                )
+              }
+            />
+          );
         }}
         keyExtractor={(item) => item}
         ItemSeparatorComponent={() => <RowSeparator />}
--- App/screens/Home.js
+++ App/screens/Home.js
@@ -101,14 +101,14 @@ export default ({navigation}) => {
       <ConversionInput
         text={baseCurrency}
         value="123"
-        onButtonPress={() => navigation.push('CurrencyList', {title: 'Base Currency'})}
+        onButtonPress={() => navigation.push('CurrencyList', {title: 'Base Currency', activeCurrency: baseCurrency})}
         onChangeText={(text) => console.log('text', text)}
         keyboardType="numeric"
       />
       <ConversionInput
         text={quoteCurrency}
         value="123"
-        onButtonPress={() => navigation.push('CurrencyList', {title: 'Quote Currency'})}
+        onButtonPress={() => navigation.push('CurrencyList', {title: 'Quote Currency', activeCurrency: quoteCurrency})}
         editable={false}
       />
       </View>
