bcf9e094f3776371e0536937d4597c687973fb06
munaciella <franes88@gmail.com>
2024-04-21 23:56:21 +0100

    change path to api

--- App/index.js
+++ App/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navigation from "./config/Navigation";
 import { api } from "./util/api";
 
-api()
+api('/latest?base=USD')
     .then(response => {
         console.log('response', response)
     })
--- App/util/api.js
+++ App/util/api.js
@@ -1,3 +1,13 @@
-export const api = () => {
+export const api = (_path = "") => {
+    const [path] = _path.split('?')
+    if (path.length === 0) {
+        return Promise.reject(new Error('Path is required.'))
+    }
+
+    if (path !== '/latest') {
+        return Promise.reject(new Error('Invalid path.'))
+    }
+
+    const baseCurrency = _path.split('base=')
   return Promise.resolve({ test: true });
 };
